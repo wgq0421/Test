970394ef8f3e55cfc497f7103d2a7f9c2ce1c036
wgq0421 <2550459954@qq.com>
2016-12-04 19:18:25 +0800

    first

---
+++ hello/src/main/java/example/com/test/SecondActivity.java
@@ -0,0 +1,15 @@
+package example.com.test;
+
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second2);
+        System.out.print("sdfad");
+    }
+}
