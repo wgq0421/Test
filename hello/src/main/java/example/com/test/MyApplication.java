a1a31b5ff488f9298593dbe1d103a96ffb52a80d
wgq0421 <2550459954@qq.com>
2017-01-13 17:02:47 +0800

    application

---
+++ hello/src/main/java/example/com/test/MyApplication.java
@@ -0,0 +1,17 @@
+package example.com.test;
+
+import android.app.Application;
+
+import com.antfortune.freeline.FreelineCore;
+
+/**
+ * Created by wuguoqiang on 2016/12/7.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        FreelineCore.init(this);
+        super.onCreate();
+    }
+}
