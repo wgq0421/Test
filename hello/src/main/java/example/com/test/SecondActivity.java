8033ba9eaba455c34ffcf9e70f827a5276835146
wgq0421 <2550459954@qq.com>
2017-01-13 16:54:28 +0800

    application

--- hello/src/main/java/example/com/test/SecondActivity.java
+++ hello/src/main/java/example/com/test/SecondActivity.java
@@ -1,6 +1,5 @@
 package example.com.test;
 
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,7 +8,7 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second2);
-        System.out.print("sdfad");
+        setContentView(R.layout.activity_second);
+
     }
 }
